abd57796e45ebe900dd69662c79d53b390e5a7d3
Hari3172 <126946415+Hari3172@users.noreply.github.com>
2023-09-14 23:49:03 +0530

    Delete design-patterns/design-patterns/src/com/te directory

--- design-patterns/design-patterns/src/com/te/bulider/TryBuilder.java
+++
@@ -1,8 +0,0 @@
-package com.te.bulider;
-
-public class TryBuilder {
-	public static void main(String[] args) {
-		Phone phone = new PhoneBuilder().setBattery(100).setRam(8).build();
-		System.out.println(phone);
-	}
-}
--- design-patterns/design-patterns/src/com/te/singleton/App.java
+++
@@ -1,9 +0,0 @@
-package com.te.singleton;
-
-public class App {
-	public static void main(String[] args) {
-		Singleton singleton = Singleton.getSingletonObject();
-		Singleton singleton02 = Singleton.getSingletonObject();
-		System.out.println(singleton.equals(singleton02));
-	}
-}
